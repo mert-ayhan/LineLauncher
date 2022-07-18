395d38cc1d00aa3f964aa906ab7f4dc4bb8c8e04
Mert Ayhan <com9@hotmail.com.tr>
2022-07-18 14:58:28 +0300

    test

--- ArtifactBuild.cmd
+++ ArtifactBuild.cmd
@@ -1,5 +1,13 @@
 @echo off
 pushd "%~dp0"
+cd bin/Release
+del /f *.xml
+del /f *.config
+del /f *.pdb
+del /f *.exe
+move .\Obfuscator_Output\line-launcher.exe
+rmdir Obfuscator_Output\
+cd ../..
 powershell Compress-7Zip "bin/Release" -ArchiveFileName "LineLauncher.zip" -Format Zip
 :exit
 popd
