9f4c540fd2d5a50d33b370e41210a9984429a378
Mert Ayhan <com9@hotmail.com.tr>
2022-07-18 12:25:36 +0300

    x

--- ArtifactBuild.cmd
+++ ArtifactBuild.cmd
@@ -1,7 +1,6 @@
 @echo off
 pushd "%~dp0"
-powershell Compress-7Zip "Release" -ArchiveFileName "LineLauncherX86.zip" -Format Zip
-powershell Compress-7Zip "x64\Release" -ArchiveFileName "LineLauncherX64.zip" -Format Zip
+powershell Compress-7Zip "Release" -ArchiveFileName "LineLauncher.zip" -Format Zip
 :exit
 popd
 @echo on
